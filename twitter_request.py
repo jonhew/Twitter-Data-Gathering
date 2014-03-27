e0134fc79e2aaa80e404b28c1a4648fb4e826120
Henry Millison <hmillison@gmail.com>
2014-03-27 17:03:32 -0400

    removing output and user inputs file name

--- twitter_request.py
+++ twitter_request.py
@@ -11,16 +11,14 @@ api = twitter.Api(
  access_token_key='699133-4xxJIkcC3qN3N8d7OQ8XI3J7ejsglrCvNlHs3jP9bWv',
  access_token_secret='lewo5SjitNlgDugggHCnDdpxjaGjXkhrWCJcYZ2Zc2dMB'
  )
-
-search = api.GetSearch(term='#hail', lang='en', result_type='recent', count=100, max_id='')
-outfile = open("outfile", "wb" )
+file = raw_input("Please enter name of file: ")
+outfile = open(file + ".csv", "wb" )
 writer = csv.writer(outfile)
+search = api.GetSearch(term='#hail', lang='en', result_type='recent', count=100, max_id='')
 
 for t in search:
-    print t.user.screen_name + ' (' + t.created_at + ')'
     #Add the .encode to force encoding
-    print t.text.encode('utf-8')
-    print ''
     writer.writerow([t.user.screen_name,t.created_at,t.text.encode('utf-8')])
+print "DONE!"
 outfile.close()
 
