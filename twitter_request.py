f0b110a9aae685fe23d020be669769688e2bd21a
Henry Millison <hmillison@gmail.com>
2014-04-08 15:03:01 -0400

    added header to top row of csv

--- twitter_request.py
+++ twitter_request.py
@@ -4,13 +4,14 @@ import json
 import csv
 
 
-
+#add your twitter api keys below
 api = twitter.Api(
- consumer_key='wplDp3kXN99bNPifkz9Qkg',
- consumer_secret='tp9LNWDriPqFM5EFNr1WE1cYbrwBHyvUDXghQtsnI0',
- access_token_key='699133-4xxJIkcC3qN3N8d7OQ8XI3J7ejsglrCvNlHs3jP9bWv',
- access_token_secret='lewo5SjitNlgDugggHCnDdpxjaGjXkhrWCJcYZ2Zc2dMB'
+ consumer_key='***',
+ consumer_secret='***',
+ access_token_key='***',
+ access_token_secret='***'
  )
+ 
 file = raw_input("Please enter name of file: ")
 outfile = open(file + ".csv", "wb" )
 writer = csv.writer(outfile)
@@ -19,9 +20,13 @@ p = 0
 #max sets total number of tweets returned
 max = 100
 print "in progress...",
+writer.writerow(["Screen Name","Created At","# Retweets","# Favorites","Tweet"])
+
 while p<max:
-    search = api.GetSearch(term='umich', lang='en', count='100', result_type='recent', max_id=i)
+    #change parameters of get search to determine results
+    search = api.GetSearch(term='#gameofthrones', lang='en', count='100', result_type='popular', max_id=i)
     i = search[0].id
+
     for t in search:
         #remove duplicate retweets
         if "RT" not in t.text:
